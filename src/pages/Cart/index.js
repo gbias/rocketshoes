d883f54376e278327c72254d4a0ddea90983011c
gbias <george.bias@outlook.com.br>
2021-07-01 00:38:04 -0300

    Styling the Page Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,3 +1,67 @@
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete
+} from 'react-icons/md'
+
+import * as Styled from './styles'
+
 export default function Cart() {
-  return <h1>Cart</h1>
+  return (
+    <Styled.Container>
+      <Styled.ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/04/E74-0492-304/E74-0492-304_zoom2.jpg?ts=1603739430&ims=326x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159C1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159C1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159C1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </Styled.ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Styled.Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Styled.Total>
+      </footer>
+    </Styled.Container>
+  )
 }
