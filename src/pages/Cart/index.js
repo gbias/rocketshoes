3c948fb000d261d5aef904154581a8115511c6bf
gbias <george.bias@outlook.com.br>
2021-07-12 22:20:42 -0300

    Calculating total

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -7,10 +7,11 @@ import {
 } from 'react-icons/md'
 
 import * as CartActions from '../../store/modules/cart/actions'
+import { formatPrice } from '../../util/format'
 
 import * as Styled from './styles'
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmount }) {
   function increment(product) {
     updateAmount(product.id, product.amount + 1)
   }
@@ -39,7 +40,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
               </td>
               <td>
                 <strong>{product.title}</strong>
-                <span>R$129,90</span>
+                <span>{product.priceFormatted}</span>
               </td>
               <td>
                 <div>
@@ -53,7 +54,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                 </div>
               </td>
               <td>
-                <strong>R$258,80</strong>
+                <strong>{product.subTotal}</strong>
               </td>
               <td>
                 <button
@@ -73,7 +74,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 
         <Styled.Total>
           <span>TOTAL</span>
-          <strong>R$1920,28</strong>
+          <strong>{total}</strong>
         </Styled.Total>
       </footer>
     </Styled.Container>
@@ -81,7 +82,15 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 }
 
 const mapStateToProps = (state) => ({
-  cart: state.cart
+  cart: state.cart.map((product) => ({
+    ...product,
+    subTotal: formatPrice(product.price * product.amount)
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount
+    }, 0)
+  )
 })
 
 const mapDispatchToProps = (dispatch) =>
