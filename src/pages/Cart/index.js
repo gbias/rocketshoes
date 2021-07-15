aa9532fce88988cf488e725df7681dd531c04397
gbias <george.bias@outlook.com.br>
2021-07-15 01:58:02 -0300

    Change stock

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -11,13 +11,13 @@ import { formatPrice } from '../../util/format'
 
 import * as Styled from './styles'
 
-function Cart({ cart, total, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
   function increment(product) {
-    updateAmount(product.id, product.amount + 1)
+    updateAmountRequest(product.id, product.amount + 1)
   }
 
   function decrement(product) {
-    updateAmount(product.id, product.amount - 1)
+    updateAmountRequest(product.id, product.amount - 1)
   }
 
   return (
