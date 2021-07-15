f127f63053b104c4aa10928ef38e02b6b8fe52ce
gbias <george.bias@outlook.com.br>
2021-07-15 02:07:00 -0300

    Add History to Saga

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom'
+import { Router } from 'react-router-dom'
 
 import './config/ReactotronConfig'
 
@@ -9,17 +9,18 @@ import GlobalStyle from './styles/global'
 import Header from './Components/Header'
 import Routes from './routes'
 
+import history from './services/history'
 import store from './store'
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   )
 }
